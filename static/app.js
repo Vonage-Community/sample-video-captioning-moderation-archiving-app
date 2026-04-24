91ab4b5b142e477f0a60cee76321cbd84247b257
liz-acosta <elizabeth.r.acosta@gmail.com>
2026-04-24 00:06:55 -0700

    refactor

--- static/app.js
+++ static/app.js
@@ -1,27 +1,23 @@
 let session = OT.initSession(applicationId, sessionId);
 
-session.on('streamCreated', (event) => {
-    session.subscribe(event.stream, 'subscriber');
-});
-
-session.on('sessionDisconnected', (event) => {
-    console.log('Disconnected from session:', event.reason);
-});
-
-session.on('signal', (event) => {
-    const messages = document.getElementById('messages');
-    messages.innerHTML += `<p>${event.data}</p>`;
-});
 
 session.connect(token, (error) => {
     if (error) {
         console.error('Error connecting:', error);
         return;
     }
-    if (isAdmin) {
-        const publisher = OT.initPublisher('publisher');
+    if (isAdmin === "true") {
+        const publisher = OT.initPublisher('publisher', { name: name });
         session.publish(publisher);
     }
+});
+
+session.on('streamCreated', (event) => {
+    session.subscribe(event.stream, 'subscriber');
+});
+
+session.on('sessionDisconnected', (event) => {
+    console.log('Disconnected from session:', event.reason);
 });
 
 function sendChat(event) {
@@ -32,3 +28,9 @@ function sendChat(event) {
         (error) => { if (!error) input.value = ''; }
     );
 }
+
+session.on('signal', (event) => {
+    const messages = document.getElementById('messages');
+    messages.innerHTML += `<p>${event.data}</p>`;
+});
+
